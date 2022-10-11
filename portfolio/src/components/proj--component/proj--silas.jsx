7a51955db5d0e873a78ba141a80165e5fee8f3dd
Return0Zero <69299788+Return0Zero@users.noreply.github.com>
2022-10-10 23:36:48 -0400

    2

--- portfolio/src/components/proj--component/proj--silas.jsx
+++ portfolio/src/components/proj--component/proj--silas.jsx
@@ -5,7 +5,6 @@ import PlusButton from '../../images/icons/plus-button.svg';
 import WebButton from '../../images/icons/web-button.png';
 import CodingButton from '../../images/icons/coding-button.svg';
 
-import HomeBefore from '../../images/projects/silas/home-before.png'
 import HomeAfter from '../../images/projects/silas/home-after.png'
 
 
@@ -18,14 +17,9 @@ const ProjSilas = () => {
             <div className='proj--content'>
                 <p>Silas Landscaping is a Family-Owned lawn care & cleaning company based in the DMV area (Washington D.C., Virginia, and Maryland) and my role in this project was to re-design and develop Silas Landscaping website to give it a fresh look to improve how customers see the company as well as to maintain a clean and professional image.</p>
             </div>
-            <div className='proj--two-images'> 
+            <div className='proj--one-images-component'> 
                 <div className='proj--image1'>
-                    <p>Before</p>
-                    <img src={HomeBefore} alt='homepage' className='proj--image' />
-                </div>
-                <div className='proj--image2'>
-                    <p>After</p>
-                    <img src={HomeAfter} alt='homepage' className='proj--image' />
+                    <img src={HomeAfter} alt='homepage' className='proj---one-image-co2222mponent' />
                 </div>
             </div>
             <div className='proj--buttons-container'>
