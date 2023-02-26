f88eaa86dfcc93ba6652710c7ca8c694ccce0d06
ReturnZero <69299788+Return0Zero@users.noreply.github.com>
2023-02-26 00:11:12 -0500

    1

--- portfolio/src/containers/navigation/navlinks.jsx
+++ portfolio/src/containers/navigation/navlinks.jsx
@@ -9,23 +9,20 @@ function NavLinks() {
 
   return (
     <div className="navlinks-container">
-      <div className='navigation--separation-bar'></div>
       <div className='navigation--container'>
           <a href='#aboutID' className='navigation--icon'>
-              <img src={AboutMeIcon} alt='About me icon'/>
+              <img src={AboutMeIcon} alt='About me icon'  className='hamburger-menu'/>
               <p className='navigation-text'>About</p>
           </a>
       </div>
-      <div className='navigation--separation-bar'></div>
       <div className='navigation--container'>
           <a href='#projectID' className='navigation--icon'>
-              <img src={ProjectsIcon} alt='Projects icon'/>
+              <img src={ProjectsIcon} alt='Projects icon'  className='hamburger-menu'/>
               <p className='navigation-text'>Projects</p>
           </a>
       </div>
-      <div className='navigation--separation-bar-second'></div>
-      <a href='#' className='navlinks--arrow'>
-        <img src={Arrow} alt='Arrow ' className='navlinks--arrow'/>
+      <a href='#' className='navlinks--arrow-component'>
+        <img src={Arrow} alt='Arrow ' className='hamburger-menu'/>
       </a>
     </div>
   );
